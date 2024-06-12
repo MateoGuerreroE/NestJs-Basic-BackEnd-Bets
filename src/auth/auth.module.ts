3ee969894ec3573c2f7501298a38254adfaffba4
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-06-12 01:23:34 -0500

    FT-Auth Address Minor Changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -19,7 +19,7 @@ import 'dotenv/config';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '50m' },
+        signOptions: { expiresIn: '60m' },
       }),
       inject: [ConfigService],
     }),
