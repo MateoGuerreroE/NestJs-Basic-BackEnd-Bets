b7c475fc7f308efbf971bc9f9d964eb7cd6b9a44
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-06-12 01:23:26 -0500

    FT-Auth Create Register Logic And Methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { DatabaseService } from 'src/database/database.service';
 import { AuthLoginRequest } from './types/AuthLoginRequest';
 import axios from 'axios';
 import { FirebaseFailedResponse } from './firebase/firebase.type';
+import { AuthRegisterRequest } from './types/AuthRegisterRequest';
+import { UserService } from 'src/api/user';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly databaseService: DatabaseService,
+    private readonly userService: UserService,
     private readonly jwtService: JwtService,
   ) {}
 
-  async validateCredentialsFirebase({
+  private firebaseAuthUrl: string = process.env.FIREBASE_AUTH_URL;
+  private firebaseApiKey: string = process.env.FIREBASE_API_KEY;
+
+  private async validateCredentialsFirebase({
     email,
     password,
   }: {
@@ -20,10 +24,12 @@ export class AuthService {
     password: string;
   }): Promise<void> {
     try {
-      await axios.post(
-        `${process.env.FIREBASE_AUTH_URL}?key=${process.env.FIREBASE_API_KEY}`,
-        { email, password, returnSecureToken: false },
-      );
+      const requestUrl = `${this.firebaseAuthUrl}/accounts:signInWithPassword?key=${this.firebaseApiKey}`;
+      await axios.post(requestUrl, {
+        email,
+        password,
+        returnSecureToken: false,
+      });
     } catch (error: any) {
       const { error: errorObject }: FirebaseFailedResponse =
         error.response.data;
@@ -33,13 +39,49 @@ export class AuthService {
     }
   }
 
-  async login(user: AuthLoginRequest) {
+  private async createFirebaseUser({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Promise<string> {
+    try {
+      const requestUrl = `${this.firebaseAuthUrl}/accounts:signUp?key=${this.firebaseApiKey}`;
+      const { data } = await axios.post(requestUrl, {
+        email,
+        password,
+        returnSecureToken: false,
+      });
+      return data.localId;
+    } catch (error: any) {
+      const { error: errorObject }: FirebaseFailedResponse =
+        error.response.data;
+      if (errorObject.message === 'EMAIL_EXISTS')
+        throw new UnauthorizedException('Email already registered');
+      throw new UnauthorizedException(errorObject.message);
+    }
+  }
+
+  async loginUser(user: AuthLoginRequest) {
     await this.validateCredentialsFirebase(user);
-    const userRecord = await this.databaseService.getUserByEmail(user.email);
+    const userRecord = await this.userService.getUserByUniqueAttributes({
+      emailAddress: user.email,
+    });
     if (!userRecord) throw new UnauthorizedException('Invalid credential');
     const payload = { email: user.email, sub: userRecord.userId };
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async registerUser(userToRegister: AuthRegisterRequest): Promise<string> {
+    const { emailAddress, password, ...user } = userToRegister;
+    const firebaseId = await this.createFirebaseUser({
+      email: emailAddress,
+      password,
+    });
+    await this.userService.createUser({ ...user, emailAddress, firebaseId });
+    return 'User registered, you can now login';
   }
 }
