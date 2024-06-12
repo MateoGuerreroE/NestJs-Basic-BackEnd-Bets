c1783ed56f891ff5fdc0eb43826028db30ab62d1
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-06-12 10:24:31 -0500

    FT-Docs Install And Configure Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+
+  const document = yaml.load(
+    fs.readFileSync('./src/openapi.yaml', 'utf-8'),
+  ) as any;
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
