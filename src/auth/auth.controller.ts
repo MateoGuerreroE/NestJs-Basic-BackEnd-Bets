6da64d93167cecf55f3fb360d2a083322fbd888d
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-06-12 01:22:15 -0500

    FT-Auth Create Registration Controller and Types

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthLoginRequest } from './types/AuthLoginRequest';
+import { AuthRegisterRequest } from './types/AuthRegisterRequest';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async loginUser(@Body() body: AuthLoginRequest) {
-    return this.authService.login(body);
+  async login(@Body() body: AuthLoginRequest) {
+    return this.authService.loginUser(body);
+  }
+
+  @Post('register')
+  async register(@Body() body: AuthRegisterRequest) {
+    return this.authService.registerUser(body);
   }
 }
