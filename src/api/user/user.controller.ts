17bcae75efed2b194db06cc68d8bde9264549e8d
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-06-12 21:58:27 -0500

    FT-User Expose Get Single User Endpoint

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,11 +1,11 @@
 import {
   Body,
   Controller,
-  HttpCode,
+  Get,
   HttpStatus,
   Post,
+  Query,
   Req,
-  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
@@ -14,27 +14,40 @@ import { JwtAuthGuard } from '../../auth/guards/jwt.guard';
 import { ApiRequest, RequestUser } from '../../auth/guards/guard.types';
 import { permissionsValidator } from '../helpers/PermissionValidator';
 import { ApiResponse } from '../types/ApiResponse';
+import { User, UserUniqueAttributeFilters } from 'src/database';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  @HttpCode(HttpStatus.CREATED)
   @UseGuards(JwtAuthGuard)
   async create(
     @Req() req: ApiRequest,
     @Body() userInfo: UserInput,
-  ): Promise<ApiResponse> {
+  ): Promise<ApiResponse<User>> {
     const user: RequestUser = req.user;
-    const isAuthorized = permissionsValidator(user, { create: 'user' });
-    if (!isAuthorized)
-      throw new UnauthorizedException('Not enought permissions');
+    permissionsValidator(user, { create: 'user' });
     const createdUser = await this.userService.createUser(userInfo);
     return {
       statusCode: HttpStatus.CREATED,
       payload: createdUser,
       message: 'User created successfully',
+    };
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getUser(
+    @Req() req: ApiRequest,
+    @Query() query: UserUniqueAttributeFilters,
+  ): Promise<ApiResponse<User>> {
+    const user: RequestUser = req.user;
+    permissionsValidator(user, { create: 'user' });
+    const foundUser: User = await this.userService.findUser({ ...query });
+    return {
+      statusCode: HttpStatus.OK,
+      payload: foundUser,
     };
   }
 }
