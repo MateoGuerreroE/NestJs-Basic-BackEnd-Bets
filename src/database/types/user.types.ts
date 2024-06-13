1d1ff853656440d8fb6ddf9d861720d1685c6850
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-06-12 21:58:02 -0500

    FT-User Enhance Permission Validator And Types

--- src/database/types/user.types.ts
+++ src/database/types/user.types.ts
@@ -1,9 +1,9 @@
 export type UserStatus = 'verified' | 'blocked' | 'pending';
 
-export type UserUniqueAttributeFilters = {
+export class UserUniqueAttributeFilters {
   emailAddress?: string;
   userId?: string;
   firebaseId?: string;
   username?: string;
   idNumber?: string;
-};
+}
