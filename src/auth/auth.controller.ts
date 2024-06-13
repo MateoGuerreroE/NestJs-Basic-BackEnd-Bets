ef6a919b52713f7b301370177359dcf7a8673181
Mateo Guerrero <mateof1223@gmail.com>
2024-06-12 22:24:47 -0500

    FT-Docs Create Initial Application Documentation (#9)
    * FT-Docs Include initial OpenApi.yaml definitions
    
    * FT-Docs Configure Build To Include OpenApi file
    
    * FT-Docs Install And Configure Swagger UI
    
    * FT-Docs Modify Auth Responses To Allign With Generic Api Response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthLoginRequest } from './types/AuthLoginRequest';
 import { AuthRegisterRequest } from './types/AuthRegisterRequest';
+import { ApiResponse } from 'src/api/types/ApiResponse';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body: AuthLoginRequest) {
-    return this.authService.loginUser(body);
+  @HttpCode(200)
+  async login(@Body() body: AuthLoginRequest): Promise<ApiResponse> {
+    const accessToken = await this.authService.loginUser(body);
+    return {
+      statusCode: HttpStatus.OK,
+      payload: accessToken,
+    };
   }
 
   @Post('register')
   async register(@Body() body: AuthRegisterRequest) {
-    return this.authService.registerUser(body);
+    const result = this.authService.registerUser(body);
+    return {
+      statusCode: HttpStatus.CREATED,
+      payload: result,
+    };
   }
 }
